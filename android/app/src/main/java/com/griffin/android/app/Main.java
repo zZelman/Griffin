4f4676bea89f018b68dbd261d740ec99c1a34baf
Kyle Avrett <kaavrett@gmail.com>
2016-06-18 08:44:45 +0000

    Removed the FloatingActionBar

--- android/app/src/main/java/com/griffin/android/app/Main.java
+++ android/app/src/main/java/com/griffin/android/app/Main.java
@@ -1,7 +1,6 @@
 package com.griffin.android.app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,15 +16,6 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override

5464c091ae90b189bec337cfd1400c2ed28bacb6
Kyle Avrett <kaavrett@gmail.com>
2016-07-07 17:23:46 -0600

    Wrote a very simple, but working layout

--- android/app/src/main/java/com/griffin/android/app/App.java
+++ android/app/src/main/java/com/griffin/android/app/App.java
@@ -4,14 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Main extends Activity {
+public class App extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        TextView label = new TextView(this);
-        label.setText("Hello world!");
-        
-        setContentView(label);
+        setContentView(R.layout.main);
     }
 }

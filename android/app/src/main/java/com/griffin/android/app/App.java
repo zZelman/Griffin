9615e9c80e71e502900e5abcea754eed6b156868
Kyle Avrett <kaavrett@gmail.com>
2016-07-09 01:25:04 +0100

    Android layout is now done

--- android/app/src/main/java/com/griffin/android/app/App.java
+++ android/app/src/main/java/com/griffin/android/app/App.java
@@ -1,13 +1,28 @@
 package com.griffin.android.app;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
+import android.app.*;
+import android.os.*;
+import android.widget.*;
 
 public class App extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        EditText commandText = (EditText) findViewById(R.id.commandText);
+        commandText.setEnabled(false);
+        commandText.setEnabled(true);
+        
+        TextView isServiceRunning = (TextView) findViewById(R.id.isServiceRunning);
+        isServiceRunning.setText("STOPPED");
+        
+        TextView commandOutput = (TextView) findViewById(R.id.commandOutput);
+        
+        String s = "";
+        for (int i = 0; i < 100; i++) {
+            s += "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n";
+        }
+        commandOutput.setText(s);
     }
 }
